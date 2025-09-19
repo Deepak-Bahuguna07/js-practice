0b085a6eee0265a6f22f32e9effde1f148af9679
Deepak-Bahuguna07 <bahugunadeepak817@gmail.com>
2025-09-19 13:07:15 +0530

    Create switch_case.js

---
+++ switch_case.js
@@ -0,0 +1,22 @@
+let inputedLPassword = 12345;
+
+switch (inputedLPassword) {
+    case 12345:
+        console.log("open");
+        break;
+    case 12347:
+        console.log("open");
+        break;
+    case 12334:
+        console.log("open");
+        break;
+    case 33345:
+        console.log("open");
+        break;
+    case 34345:
+        console.log("open");
+        break;
+    default:
+        console.log("Not matched");
+        break;
+}
