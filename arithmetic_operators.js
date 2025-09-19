b8156a22adcefdd6f57dec9e2b7a5bb7e2731e15
Deepak-Bahuguna07 <bahugunadeepak817@gmail.com>
2025-09-19 12:33:27 +0530

    uploaded a file  arithmetic_operators.js

---
+++ arithmetic_operators.js
@@ -0,0 +1,34 @@
+let a = 10;
+let b = 5;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log();
+
+// synthetic sugar
+a = 10;
+a += 10;
+console.log(a);
+a -= 10;
+console.log(a);
+a *= 10;
+console.log(a);
+a /= 10;
+console.log(a);
+a %= 10;
+console.log(a);
+console.log();
+
+a = 10;
+let c = a + b;
+console.log(c);
+console.log(c == a + b);
+console.log();
+
+let d=a++;
+console.log(d);
+console.log(a);
+
