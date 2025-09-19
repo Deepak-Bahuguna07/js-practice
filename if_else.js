d49882bb81c18c56e766d22cbcbcd5756e1888d4
Deepak-Bahuguna07 <bahugunadeepak817@gmail.com>
2025-09-19 12:46:32 +0530

    Create if_else.js

---
+++ if_else.js
@@ -0,0 +1,38 @@
+if (10===1){
+    console.log("running..");   
+}
+ else{
+    console.log("Else running");
+}
+
+let A=23;
+(A%2==0)?"Even":"odd";
+
+let a = 23;
+console.log(a);
+{
+    let a = 34
+    console.log(a);
+}
+console.log(a);
+
+let dayNumber = 7;
+
+if (dayNumber === 1) {
+    console.log("monday");
+} else if (dayNumber === 2) {
+    console.log("tuesday");
+} else if (dayNumber === 3) {
+    console.log("wednesday");
+} else if (dayNumber === 4) {
+    console.log("thursday");
+} else if (dayNumber === 5) {
+    console.log("friday");
+} else if (dayNumber === 6) {
+    console.log("saturday");
+} else if (dayNumber === 7) {
+    console.log("sunday");
+} else {
+    console.log("No Vailid Number");
+}
+
