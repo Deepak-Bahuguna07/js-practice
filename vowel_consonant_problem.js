ad13de6ca7b3040da67314cf37deacde029bd766
Deepak-Bahuguna07 <bahugunadeepak817@gmail.com>
2025-09-27 15:34:54 +0530

    created functions.

--- vowel_consonant_problem.js
+++ vowel_consonant_problem.js
@@ -1,1 +1,20 @@
+function convert(text) {
 
+}
+
+function displayMessage(message) {
+  console.log(message);
+}
+
+function testResult(text, expectedAns) {
+  const actual = convert(text);
+  const symbol = actual === expectedAns ? "✅" : "❌";
+  const message = symbol + "expected :" + expectedAns + " | actual :" + actual;
+  displayMessage(message);
+}
+
+function testAll() {
+  
+}
+
+testAll();
