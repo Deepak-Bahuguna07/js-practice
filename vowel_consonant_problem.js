621d2dc3a47c702eb2bb5c39095eed2dc1e249f7
Deepak-Bahuguna07 <bahugunadeepak817@gmail.com>
2025-09-28 12:37:20 +0530

    Update vowel_consonant_problem.js

--- vowel_consonant_problem.js
+++ vowel_consonant_problem.js
@@ -1,28 +1,38 @@
 function isVowel(charecter) {
-  return charecter === 'a' || charecter === 'e' || charecter === 'i' || charecter === 'o' || charecter === 'u';
+  switch (charecter) {
+    case 'a':
+      return true;
+    case 'e':
+      return true;
+    case 'i':
+      return true;
+    case 'o':
+      return true;
+    case 'u':
+      return true;
+    default:
+      return false;
+  }
 }
 
-function isConsonant(charecter) {
-  return !isVowel(charecter);
-}
-
-function isDiffrent(lastCharecter, currentCharecter) {
-  return isVowel(lastCharecter) !== isVowel(currentCharecter);
+// for checking both charecters are diffrent 1 is vowel 1 is consonante
+function isDiffrentType(previousCharecter, currentCharecter) {
+  return isVowel(previousCharecter) !== isVowel(currentCharecter);
 }
 
 function convert(candidateString) {
   let convertedStr = "";
-  let text = candidateString;
+  let currentCandidate = candidateString;
 
-  while (text.length > 1) {
-    convertedStr += text[0];
+  while (currentCandidate.length > 0) {
+    convertedStr += currentCandidate[0];
     let convertedSubStr = "";
 
-    for (let index = 1; index < text.length; index++) {
-      if (isDiffrent(convertedStr[convertedStr.length - 1], text[index])) {
-        convertedStr += text[index];
+    for (let index = 1; index < currentCandidate.length; index++) {
+      if (isDiffrentType(convertedStr[convertedStr.length - 1], currentCandidate[index])) {
+        convertedStr += currentCandidate[index];
       } else {
-        convertedSubStr += text[index];
+        convertedSubStr += currentCandidate[index];
       }
     }
 
@@ -30,10 +40,10 @@ function convert(candidateString) {
       convertedStr += ",";
     }
 
-    text = convertedSubStr;
+    currentCandidate = convertedSubStr;
   }
 
-  return convertedStr + text;
+  return convertedStr;
 }
 
 function displayMessage(message) {
@@ -52,10 +62,13 @@ function testAll() {
   testResult("hello", "helo,l");
   testResult("apple", "ape,p,l");
   testResult("there", "tere,h");
+  testResult("poetre", "pote,er");
+  testResult("poetee", "pote,e,e");
   testResult("abyss", "ab,y,s,s");
   testResult("aaaeis", "as,a,a,e,i");
   testResult("aaabbb", "ab,ab,ab");
-  testResult("", "");
+  testResult("aaabbbaaa", "aba,aba,aba");
+  testResult("aaaiieeooouu", "a,a,a,i,i,e,e,o,o,o,u,u");
 }
 
 testAll();
