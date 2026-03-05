779adb60dd80ad5c5a2d9bc5f9e7a6001bd60664
Deepak Bahuguna <deepakbahuguna@Deepaks-MacBook-Pro.local>
2026-03-05 17:57:13 +0530

    changes some stylings

--- server/pokedex/public/scripts/eventListeners.js
+++ server/pokedex/public/scripts/eventListeners.js
@@ -10,6 +10,28 @@ const serveSpecificTypePokemons = (type, container, pokemons) => {
   renderCards(container, filterdPokemons);
 }
 
+const showWarning = (message) => {
+  let warning = document.createElement('div');
+  warning.textContent = message;
+  warning.style.position = 'absolute';
+  warning.style.top = '0.5em';
+  warning.style.left = "0.5em";
+  warning.style.backgroundColor = 'red';
+  warning.style.color = 'white';
+  warning.style.padding = '1em';
+  warning.style.borderRadius = '4px';
+  warning.style.zIndex = '1000';
+  warning.style.fontSize = '1em';
+  warning.style.zIndex = '1';
+  warning.style.pointerEvents = 'none';
+
+  document.querySelector('header').appendChild(warning);
+  console.log("hey")
+  setTimeout(() => {
+    warning.remove();
+  }, 1000);
+}
+
 export const attechSearchBtnEvent = (container, pokemons) => {
   const form = document.querySelector("#search-pokemon-name");
 
@@ -18,7 +40,12 @@ export const attechSearchBtnEvent = (container, pokemons) => {
 
     const fd = new FormData(form);
     const { name } = Object.fromEntries(fd.entries());
-    const pokemon = pokemons[name] ? pokemons[name] : pokemons["ditto"];
+    const pokemon = pokemons[name];
+    //  ? pokemons[name] : pokemons["ditto"];
+    if (pokemon === undefined) {
+      return showWarning('Pokemon Not Found!');
+    }
+
     container.innerHTML = "";
 
     const card = generateCard(pokemon);
